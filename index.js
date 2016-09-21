6c6221d18879759d63f9970884afbd3a23ae7fb7
Haowen <zt452268020@live.com>
2016-09-21 12:31:12 +0800

    add koa-static-server

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+let serve = require('koa-static-server');
 let koa = require('koa');
+
 let app = koa();
 
-app.use(function *(){
-  this.body = 'Hello World';
-});
-
+// serve static
+app.use(serve({rootDir: 'client/dist'}));
+app.use(function* (next) {
+  console.log(this.request)
+})
+app.on('error', (err) => {
+  console.log(err)
+})
 app.listen(3000);
 console.log('listening at 3000');
