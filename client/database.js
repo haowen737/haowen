8176476f5e2661432f4320251a64f9abcd1bee12
Haowen <zt452268020@live.com>
2016-09-23 19:21:11 +0800

    delete test data

--- client/database.js
+++ client/database.js
@@ -4,66 +4,6 @@ let data = {
     keywords: 'hello-world',
     date: '2016-9-22',
     tags: ['hello', '要做的事情']
-  }, {
-    title: 'test-1',
-    keywords: 'test-1',
-    date: '2016-9-22',
-    tags: ['test-1', 'ha']
-  }, {
-    title: 'test-2',
-    keywords: 'test-2',
-    date: '2016-9-22',
-    tags: ['蓝卡', 'haha']
-  }, {
-    title: 'test-2',
-    keywords: 'test-2',
-    date: '2016-9-22',
-    tags: ['阿斯顿-2', '神秘']
-  }, {
-    title: 'test-2',
-    keywords: 'test-2',
-    date: '2016-9-22',
-    tags: ['奥古斯-2', '天空']
-  }, {
-    title: 'test-1',
-    keywords: 'test-1',
-    date: '2016-9-22',
-    tags: ['test-1', 'ha']
-  }, {
-    title: 'test-2',
-    keywords: 'test-2',
-    date: '2016-9-22',
-    tags: ['蓝卡', 'haha']
-  }, {
-    title: 'test-2',
-    keywords: 'test-2',
-    date: '2016-9-22',
-    tags: ['阿斯顿-2', '神秘']
-  }, {
-    title: 'test-2',
-    keywords: 'test-2',
-    date: '2016-9-22',
-    tags: ['奥古斯-2', '天空']
-  }, {
-    title: 'test-1',
-    keywords: 'test-1',
-    date: '2016-9-22',
-    tags: ['test-1', 'ha']
-  }, {
-    title: 'test-2',
-    keywords: 'test-2',
-    date: '2016-9-22',
-    tags: ['蓝卡', 'haha']
-  }, {
-    title: 'test-2',
-    keywords: 'test-2',
-    date: '2016-9-22',
-    tags: ['阿斯顿-2', '神秘']
-  }, {
-    title: 'test-2',
-    keywords: 'test-2',
-    date: '2016-9-22',
-    tags: ['奥古斯-2', '天空']
   }]
 }
 export default data
