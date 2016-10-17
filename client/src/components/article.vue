f8e58824c67fc9ceeb9458f2c83bbbea17551ad1
Haowen <zt452268020@live.com>
2016-10-17 22:59:11 +0800

    add bottom fire in article

--- client/src/components/article.vue
+++ client/src/components/article.vue
@@ -1,5 +1,5 @@
 <template lang="html">
-  <div class="article-layout">
+  <div class="article-layout" id="container">
     <header>
       <div class="logo cur-default">Haowen</div>
       <hr>
@@ -10,13 +10,16 @@
       </nav>
     </header>
     <div v-html="markdown" class="markdown-body"></div>
+    <bottom-fire :show.sync="showBottomFire"></bottom-fire>
   </div>
 </template>
 
 <script>
+import bottomFire from './../directive/bottomFire'
 export default {
   data () {
     return {
+      showBottomFire: false,
       article: '',
       markdown: ''
     }
@@ -31,6 +34,7 @@ export default {
       let title = this.$route.params.id
       console.log(title)
       this.query(title)
+      this.watchBottom()
     },
     query (title) {
       this
@@ -47,9 +51,30 @@ export default {
     formatMarkdown () {
       console.log(this.article)
       this.markdown = window.markdown.toHTML(this.article)
+    },
+    watchBottom () {
+      this.$nextTick(function () {
+        window.onscroll = () => {
+          let offsetHeight = document.getElementById('container').offsetHeight
+          let innerHeight = window.innerHeight
+          let scrollY = window.scrollY
+          console.log(offsetHeight, innerHeight, scrollY)
+          // 160为cards的margin-bottom
+          // this.topScrollbarWidth.width = (scrollY / (offsetHeight + 160 - innerHeight)) * 100 + '%'
+          if ((innerHeight + scrollY) - offsetHeight > -70) {
+            console.log(true)
+            this.showBottomFire = true
+          } else {
+            console.log(false)
+            this.showBottomFire = false
+          }
+        }
+      })
     }
   },
-  components: {}
+  components: {
+    bottomFire
+  }
 }
 </script>
 
@@ -64,7 +89,7 @@ export default {
   box-sizing: border-box;
   min-width: 200px;
   max-width: 900px;
-  margin: 0 auto;
+  margin: 0 auto 100px;
   padding: 45px;
 }
 .article {
