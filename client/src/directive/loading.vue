fd813b66f7d71877e2f089dbe69a29f8badf4612
郑皓文 <Haowen@zhenghawendeMBP.lan>
2016-11-12 22:14:42 +0800

    rm vid from assets

--- client/src/directive/loading.vue
+++ client/src/directive/loading.vue
@@ -34,6 +34,7 @@ export default {
   height: 10px;
   width: 100px;
   left: 50%;
+  -webkit-transform: translateX(-50%);
   transform: translateX(-50%);
 }
 .ball {
@@ -47,16 +48,76 @@ export default {
           filter: blur(1px);
 }
 .ball-1 {
+  -webkit-animation: ball-1 3s infinite;
   animation: ball-1 3s infinite;
 }
 .ball-2 {
+  -webkit-animation: ball-2 3s infinite;
   animation: ball-2 3s infinite;
 }
 .ball-3 {
+  -webkit-animation: ball-3 3s infinite;
   animation: ball-3 3s infinite;
 }
 .ball-4 {
+  -webkit-animation: ball-4 3s infinite;
   animation: ball-4 3s infinite;
+}
+@-webkit-keyframes ball-1 {
+  0% {
+    left:0px;
+  }
+  50% {
+    left: 100px;
+  }
+  100% {
+    left:0px;
+  }
+}
+@-webkit-keyframes ball-2 {
+  0% {
+    left:100px;
+  }
+  50% {
+    left: 0px;
+  }
+  100% {
+    left:100px;
+  }
+}
+@-webkit-keyframes ball-3 {
+  0% {
+    left:0px;
+  }
+  25% {
+    left: 100px;
+  }
+  50% {
+    left: 0px;
+  }
+  75% {
+    left: 100px;
+  }
+  100% {
+    left: 0px;
+  }
+}
+@-webkit-keyframes ball-4 {
+  0% {
+    left: 100px;
+  }
+  25% {
+    left: 0px;
+  }
+  50% {
+    left: 100px;
+  }
+  75% {
+    left: 0px;
+  }
+  100% {
+    left: 100px;
+  }
 }
 @keyframes ball-1 {
   0% {
