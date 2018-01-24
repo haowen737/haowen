772940b8b26d21a1032bfb0ccf06dc3a80024a15
Haowen <zt452268020@live.com>
2018-01-24 17:01:28 +0800

    feat: add mail notify when reply

--- controllers/comment.js
+++ controllers/comment.js
@@ -64,7 +64,7 @@ exports.addComment = async (ctx, next) => {
   //   .first()
 
   if (!user) {
-    throw new ApiError({code: 10004, msg: '缺少用户资料'})
+    throw new ApiError({ code: 10004, msg: '缺少用户资料' })
   }
 
   await ctx.knex('comments').insert({
@@ -85,21 +85,42 @@ exports.addComment = async (ctx, next) => {
  }
 
 exports.addReply = async (ctx, next) => {
-  let userId = ctx.request.body.userId
-  let content = ctx.request.body.content
-  let parentId = ctx.request.body.parentId || null
+  const userId = ctx.request.body.userId
+  const content = ctx.request.body.content
+  const parentId = ctx.request.body.parentId || null
   let replyToUser = ctx.request.body.replyToUser || null
-  let date = new Date()
+  const date = new Date()
 
-  let user = await ctx.knex('users as u')
-    .select('u.id')
+  const user = await ctx.knex('users as u')
+    .select()
     .where('id', userId)
     .first()
 
 
   if (!user.id) {// 未查询到的新用户插入users表
-    throw new ApiError({code: 10004, msg: '缺少用户资料'})
+    throw new ApiError({ code: 10004, msg: '缺少用户资料' })
   }
+
+  if (!replyToUser) {
+    // 回复comment表中的评论
+    const parentComment = await ctx.knex('comments')
+      .select()
+      .where('id', parentId)
+      .first()
+
+    replyToUser = parentComment.user_id
+  }
+
+  const targetUser = await ctx.knex('users as u')
+    .select()
+    .where('id', replyToUser)
+    .first()
+
+  MailSender({
+    to: targetUser.email,
+    subject: '来自withyoufriends的新回复', // Subject line
+    text: `用户名：${user.nick_name}对你回复说：${content}` // plain text body
+  })
 
   await ctx.knex('replys').insert({
     user_id: userId,
@@ -107,11 +128,6 @@ exports.addReply = async (ctx, next) => {
     created_at: date,
     parent_id: parentId,
     reply_to: replyToUser
-  })
-
-  MailSender({
-    subject: '收到新回复', // Subject line
-    text: `用户名：${user.nick_name}对${replyToUser}回复说：${content}` // plain text body
   })
 
   ctx.body = {
